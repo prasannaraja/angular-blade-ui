e8ade0dca14bb3b4403152cc98d68fab8f50392e
Prasanna Prabhakaran <prasannaraja@msn.com>
2021-01-30 18:04:17 +0530

    blade component updated

---
+++ blade-ui/src/app/shared/blader/blade/blade.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blade',
+  templateUrl: './blade.component.html',
+  styleUrls: ['./blade.component.scss']
+})
+export class BladeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
