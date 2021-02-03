d5422709d59a44706f7b944a94da5eb52701ce87
Prasanna Prabhakaran <prasannaraja@msn.com>
2021-02-03 20:46:30 +0530

    intermediate commit

--- blade-ui/src/app/features/welcome/welcome.module.ts
+++ blade-ui/src/app/features/welcome/welcome.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { WelcomeComponent } from '../welcome/welcome.component';
 import { WelcomeRoutingModule } from './welcome-routing.module';
 import { DashboardModule } from 'src/app/shared/controls/dashboard/dashboard.module';
+import { LazyGridModule } from 'src/app/shared/controls/lazy-grid/lazy-grid.module';
 
 @NgModule({
   declarations: [WelcomeComponent],
   exports: [WelcomeComponent],
-  imports: [CommonModule, WelcomeRoutingModule, DashboardModule],
+  imports: [CommonModule, WelcomeRoutingModule, DashboardModule, LazyGridModule],
 })
 export class WelcomeModule {}
