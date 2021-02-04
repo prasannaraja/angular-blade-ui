346c592400156616d3e3bc28236cc13f4c9365cc
Prasanna Raja <prasannaraja@msn.com>
2021-02-04 06:37:53 +0530

    added dummy content to dashboard

--- blade-ui/src/app/shared/controls/dashboard/dashboard.module.ts
+++ blade-ui/src/app/shared/controls/dashboard/dashboard.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [DashboardComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatDividerModule, MatGridListModule],
   exports:[DashboardComponent]
 })
 export class DashboardModule { }
